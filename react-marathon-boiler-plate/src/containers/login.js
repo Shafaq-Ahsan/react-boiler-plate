c249aa9bbc9e4054bb6e40ea66930c6a58048467
h4s33b <haseeb.rehman@iunc.edu.pk>
2017-03-02 14:22:04 +0500

    Initial Commit for marathon boiler-plate. (Till Login Forms)

---
+++ react-marathon-boiler-plate/src/containers/login.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import Login from '../components/login/login';
+import { loadInitialState } from '../store/actions/loadInitialState';
+
+function mapStateToProps(state) {
+  //here we are mapping the redux state to props so we can use it in our components
+  return {
+    counter: state.counter
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  //Those will be the actions we will be Triggerening from Components
+  return {
+    loadInitialState    : () => dispatch(loadInitialState()),
+  };
+}
+
+const LoginContainer = connect(mapStateToProps, mapDispatchToProps)(Login);
+
+export default LoginContainer;
---
+++ react-marathon-boiler-plate/src/containers/signup.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import SignUp from '../components/signUp/signup';
+import { loadInitialState } from '../store/actions/loadInitialState';
+
+function mapStateToProps(state) {
+  //here we are mapping the redux state to props so we can use it in our components
+  return {
+    counter: state.counter
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  //Those will be the actions we will be Triggerening from Components
+  return {
+    loadInitialState    : () => dispatch(loadInitialState()),
+  };
+}
+
+const SignUpContainer = connect(mapStateToProps, mapDispatchToProps)(SignUp);
+
+export default SignUpContainer;
